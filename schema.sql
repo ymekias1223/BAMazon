ee8393e950c2ae7907fa56332c024ff72cf08dc9
Your Name <you@example.com>
2018-02-23 23:55:05 -0800

    second commit. Added the sql schema

--- schema.sql
+++ schema.sql
@@ -1,2 +1,14 @@
+CREATE DATABASE bamazon;
+USE bamazon;
+CREATE TABLE products(
+id integer auto_increment not null primary key,
+name varchar(150),
+department varchar(150),
+price decimal,
+quantity integer not null,
+product_sales decimal not null
+);
 
 
+#after this you would probably want to seed the database with some entries.  I create the database 
+#on the cmd line 
